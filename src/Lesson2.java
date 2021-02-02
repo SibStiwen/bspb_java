cd7b39008d9d67cb0fa02a0184ee4dd40875cc2b
Stiwen <stiwennsk@yandex.ru>
2021-02-02 23:59:25 +0300

    Finished

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,13 +1,16 @@
-// Пока не закончил, закоммитил случайно
-
 public class Lesson2 {
     private static String[] names = new String[10];
     private static boolean[] sexes = new boolean[10];
     private static int[] ages = new int[10];
-    private static int[] indexes = new int[10];
+
+    private static String[] outNames = new String[10];
+    private static boolean[] outSexes = new boolean[10];
+    private static int[] outAges = new int[10];
+
     private static int counter = 0;
+    private static int outCounter =0;
 
-
+// Заполним массивы данными
     public static void main(String[] args) {
         newClient("Беляев Александр Александрович", true,36);
         newClient("Щербакова Мария Лукинична", false,19);
@@ -20,13 +23,20 @@ public class Lesson2 {
         newClient("Макаров Павел Робертович",true,30);
         newClient("Антонов Григорий Кириллович", true,44);
 
+// Отфильтруем по полу до сортировки, данные выведем в результирующие массивы
+        filterClients(true);
 
-        for (int i = 0; i < names.length; i++) {
+// Отсортируем по возрасту
+        sortClientsAge();
+
+// Выведем, указав пол буквой
+        for (int i = 0; i < outCounter; i++) {
             char sex = 'Ж';
-            if (sexes[i]){
+            if (outSexes[i]) {
                 sex = 'М';
             }
-            System.out.println(names[i]+" "+sex+" "+ages[i]);
+            System.out.printf("%s %s %s %n", outNames[i], sex, outAges[i]);
+//            System.out.println(outNames[i]+" "+sex+" "+outAges[i]);
 
         }
 
@@ -39,18 +49,36 @@ public class Lesson2 {
         names[counter] = name;
         sexes[counter] = sex;
         ages[counter] = age;
-        indexes[counter] = counter;
         counter++;
     }
 
-    public static void sortClientsAge () {
-        for(int i = ages.length-1 ; i > 0 ; i--){
-            for(int j = 0 ; j < i ; j++){
-                if( ages[j] > ages[j+1] ){
-                    int tmp = ages[j];
-                    ages[j] = ages[j+1];
-                    ages[j+1] = tmp;
+    public static void sortClientsAge() {
+        for (int i = outCounter - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (outAges[j] > outAges[j + 1]) {
+                    int tmp = outAges[j];
+                    outAges[j] = outAges[j + 1];
+                    outAges[j + 1] = tmp;
+                    boolean tmp2 = outSexes[j];
+                    outSexes[j] = outSexes[j + 1];
+                    outSexes[j + 1] = tmp2;
+                    String tmp3 = outNames[j];
+                    outNames[j] = outNames[j + 1];
+                    outNames[j + 1] = tmp3;
                 }
+            }
+        }
+
+
+    }
+    public static void filterClients (boolean isMan) {
+        for (int i = 0; i < sexes.length; i++){
+            if (sexes[i] == isMan){
+                outNames[outCounter] = names[i];
+                outSexes[outCounter] = sexes[i];
+                outAges[outCounter] = ages[i];
+                outCounter++;
+
             }
         }
 
